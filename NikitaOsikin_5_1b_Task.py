9407baa87e8e614f361ff172a945645b54c7ac96
Nikita <108076704+NikitaOchenkrut@users.noreply.github.com>
2022-07-21 20:50:35 +0600

    Add files via upload
    Работа с словарями:
    Вывод по заданному значению.
    Вывод значения из словаря

---
+++ NikitaOsikin_5_1b_Task.py
@@ -0,0 +1,31 @@
+# Задача 5.1в. Вывод значения словаря.
+london_co = {
+    "r1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.1"
+    },
+    "r2": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "4451",
+        "ios": "15.4",
+        "ip": "10.255.0.2"
+    },
+    "sw1": {
+        "location": "21 New Globe Walk",
+        "vendor": "Cisco",
+        "model": "3850",
+        "ios": "3.6.XE",
+        "ip": "10.255.0.101",
+        "vlans": "10,20,30",
+        "routing": True
+    }
+}
+
+exam_key = input("Введите имя устройства: ")
+print("Введите имя параметра", list(london_co[exam_key].keys()), " : ")
+exam_key2 = input()
+print(london_co[exam_key][exam_key2])
