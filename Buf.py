788b75a9d9c0fca03977dbb2c4888348ec540b77
Nikita <108076704+NikitaOchenkrut@users.noreply.github.com>
2022-07-24 18:58:10 +0600

    Add files via upload
    Цикл Fifo

---
+++ Buf.py
@@ -0,0 +1,16 @@
+class buff:
+
+    def fifo(matrix):
+        for i in range(len(matrix)):
+            matrix.pop(0)
+            print(matrix)
+
+        return matrix
+
+
+size = int(input("Сколько элементов массива потребуется?: "))
+matrixOne = []
+for i in range(size):
+    matrixOne.append(int(input(f"Введите {i} число списка: ")))
+
+buff.fifo(matrix=matrixOne)
